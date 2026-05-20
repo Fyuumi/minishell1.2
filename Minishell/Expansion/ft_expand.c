82851b1d761e0a45e174bab8c8cd352d3f974f12
Fyuumi <73714037+Fyuumi@users.noreply.github.com>
2026-05-20 12:40:07 +0000

    sorted

--- Minishell/Expansion/ft_expand.c
+++ Minishell/Expansion/ft_expand.c
@@ -15,3 +15,28 @@
 /*ft_expand_cmds + ft_expand_str. Replaces $ VAR and $ ?, handles ' literal
 and " semi-literal, then strips the quote characters.*/
 
+void ft_expand_cmds(t_cmd *cmds, t_env *env, int last_status)
+{
+    char **str;
+    char *line;
+    char *new;
+    int i;
+    int flag;
+
+    flag = 0;
+    str = cmds->argv;
+    line = str[0];
+    new = malloc(ft_strlen(line) + 1);
+    while(line[i])
+    {
+        
+        if (line[i] == '"' && flag == 0)
+        {
+            flag = 1;
+             i++;
+        }
+            i++;
+        new[i] = line[i];
+        i++;
+    }
+}
